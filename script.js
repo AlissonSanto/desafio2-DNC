9d89e823430f3f5fd6357118fae3854cf490637d
AlissonSanto <138172288+AlissonSanto@users.noreply.github.com>
2024-03-18 22:31:57 -0300

    adicionando API de tempo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-async function getAddressByCep() {
+async function pegarCep() {
     const cep = document.getElementById("cep").value
 
     try {
@@ -14,4 +14,22 @@ async function getAddressByCep() {
     } catch (error) {
         console.log(error.message)
     }
+
+
+    const lat = document.getElementById("lat").value
+    const log = document.getElementById("log").value
+
+
+    try {
+        const response = await fetch(
+            `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${log}&current=temperature_2m&hourly=temperature_2m&timezone=America%2FSao_Paulo&forecast_days=1`);
+        const data = await response.json();
+        console.log(data)
+        document.getElementById('temperatura').value = data.hourly.temperature_2m;
+        
+
+    } catch (error) {
+        console.log(error.message)
+    }
 }
+
